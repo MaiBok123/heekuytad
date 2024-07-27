e5287d3e74f27a042b8092e11e7e2b4f1d80eeb3
I_don't_tell_u <133332005+MaiBok123@users.noreply.github.com>
2024-07-27 17:41:43 +0700

    Add files via upload

---
+++ collect_imgs.py
@@ -0,0 +1,44 @@
+import os
+import cv2
+import time
+
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+number_of_classes = 5
+dataset_size = 1000
+interval = 10  # เวลาที่ต้องการในการเก็บภาพ (วินาที)
+
+cap = cv2.VideoCapture(0)
+for j in range(number_of_classes):
+    if not os.path.exists(os.path.join(DATA_DIR, str(j))):
+        os.makedirs(os.path.join(DATA_DIR, str(j)))
+
+    print(f'Collecting data for class {j}')
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    start_time = time.time()  # เริ่มนับเวลาตอนนี้
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        cv2.imshow('frame', frame)
+        cv2.waitKey(1)
+        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
+
+        counter += 1
+
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= interval:
+            break
+
+cap.release()
+cv2.destroyAllWindows()
